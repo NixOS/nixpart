0f9cdf707bab1e43b8c8a35bc875ac9b21b6ec22
aszlig <aszlig@redmoonstudios.org>
2013-06-27 17:43:14 +0200

    Rename project to "nixpart".
    This name is more appropriate, as our intention is to get rid off the kickstart
    syntax.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup(name='nixkickstart',
+setup(name='nixpart',
       version='0.1',
-      scripts=["kickstart"],
+      scripts=["nixpart"],
       py_modules=["nixkickstart"])
