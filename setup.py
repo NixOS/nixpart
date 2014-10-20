f06f5499ffcbfd9ff6e01170c692b2b4eee0330e
aszlig <aszlig@redmoonstudios.org>
2014-10-20 06:06:51 +0200

    Improve setup.py and restructure directory layout.
    We're preparing for nixpart 1.0, and we'll start by creating a better
    directory layout, especially because we're going to implement unit tests
    from the beginning.
    
    Also the fields provided in setup.py were a bit terse and are a bit more
    comprehensional now.
    
    Note that the license still is GPL and we're going to change to MIT
    license as soon as we got rid of all the code from Anaconda.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- setup.py
+++ setup.py
@@ -1,6 +1,33 @@
-from distutils.core import setup
+#!/usr/bin/env python
+import sys
+
+from distutils.core import setup, Command
+from unittest import TextTestRunner, TestLoader
+
+PYTHON_MODULES = [
+    'nixpart',
+    'nixpart.nixkickstart',
+    'nixpart.tests',
+]
+
+
+class RunTests(Command):
+    description = 'run test suite'
+    user_options = []
+    initialize_options = finalize_options = lambda self: None
+
+    def run(self):
+        tests = TestLoader().loadTestsFromName('nixpart.tests')
+        result = TextTestRunner(verbosity=1).run(tests)
+        sys.exit(not result.wasSuccessful())
 
 setup(name='nixpart',
-      version='0.4.0',
-      scripts=["nixpart"],
-      py_modules=["nixkickstart"])
+      version='1.0.0',
+      description='NixOS storage manager/partitioner',
+      url='https://github.com/aszlig/nixpart',
+      author='aszlig',
+      author_email='aszlig@redmoonstudios.org',
+      scripts=['scripts/nixpart'],
+      py_modules=PYTHON_MODULES,
+      cmdclass={'test': RunTests},
+      license='GPL')
