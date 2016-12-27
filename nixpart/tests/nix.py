ae33d6987c069cd636af355a6243283e0bef036c
aszlig <aszlig@redmoonstudios.org>
2016-12-27 18:46:49 +0100

    tests/nix: Add tests with a fake module system
    We now have one NixConfigTest class which contains all of the tests and
    two subclasses which either run these tests with the real module system
    (ParseFullNixConfigTest) or a fake module system
    (ParseMockNixConfigTest) that's just passing through everything that has
    been defined in the configuration as-is.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- nixpart/tests/nix.py
+++ nixpart/tests/nix.py
@@ -22,7 +22,7 @@ def has_nixpkgs():
     return False
 
 
-class ParseNixConfigTest(unittest.TestCase):
+class NixConfigTest(unittest.TestCase):
     def setUp(self):
         self.statedir = tempfile.mkdtemp()
         self.old_env = os.environ.copy()
@@ -32,7 +32,6 @@ class ParseNixConfigTest(unittest.TestCase):
         shutil.rmtree(self.statedir, ignore_errors=True)
         os.environ = self.old_env
 
-    @unittest.skipUnless(has_nixpkgs(), "no <nixpkgs> available")
     def test_full_nixpkgs(self):
         simple_cfg = b'''
         {
@@ -68,3 +67,39 @@ class ParseNixConfigTest(unittest.TestCase):
                              result['fileSystems']['/']['storage'])
 
             self.assertNotIn('networking', result)
+
+
+@unittest.skipUnless(has_nixpkgs(), "no <nixpkgs> available")
+class ParseFullNixConfigTest(NixConfigTest):
+    pass
+
+
+class ParseMockNixConfigTest(NixConfigTest):
+    def setUp(self):
+        super().setUp()
+        self.dummy_nixpkgs_dir = tempfile.mkdtemp()
+        eval_config_path = os.path.join(self.dummy_nixpkgs_dir,
+                                        'nixos', 'lib', 'eval-config.nix')
+        os.makedirs(os.path.dirname(eval_config_path))
+        open(eval_config_path, 'wb').write(b'''
+          { modules }: let
+            module = builtins.head modules;
+            isPlain = builtins.isAttrs module || builtins.isFunction module;
+            attrs = { pkgs = {}; lib = {}; config = {}; options = {}; };
+            callMod = mod: if builtins.isAttrs mod then mod else mod attrs;
+            eval = callMod (if isPlain then module else import module);
+            augmented = eval // {
+              fileSystems = eval.fileSystems or {};
+              swapDevices = eval.swapDevices or {};
+              storage = eval.storage or {};
+            };
+          in { config = augmented; }
+        ''')
+        os.environ['NIX_PATH'] = "nixpkgs={}".format(self.dummy_nixpkgs_dir)
+
+    def tearDown(self):
+        shutil.rmtree(self.dummy_nixpkgs_dir)
+        super().tearDown()
+
+# Make sure the NixConfigTest class isn't directly run by the test runner.
+del NixConfigTest
