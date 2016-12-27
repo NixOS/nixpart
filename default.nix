601b934c34714948e59f55e9775782064372787d
aszlig <aszlig@redmoonstudios.org>
2016-12-27 17:44:49 +0100

    default.nix: Add option to run with full <nixpkgs>
    This passes a NIX_PATH with a full import of <nixpkgs> to the builder so
    that we can run the ParseNixConfigTest.test_full_nixpkgs test.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- default.nix
+++ default.nix
@@ -1,11 +1,16 @@
+{ runAllTests ? false }:
+
 with import <nixpkgs> {};
 
-python3Packages.buildPythonApplication rec {
+python3Packages.buildPythonApplication (rec {
   name = "nixpart-${version}";
   version = "1.0.0";
 
   src = ./.;
 
+  checkInputs = [ nix ];
   propagatedBuildInputs = [ python3Packages.blivet ];
   makeWrapperArgs = [ "--set GI_TYPELIB_PATH \"$GI_TYPELIB_PATH\"" ];
-}
+} // lib.optionalAttrs runAllTests {
+  NIX_PATH = "nixpkgs=${<nixpkgs>}";
+})
