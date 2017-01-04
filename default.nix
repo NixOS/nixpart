fafd2867af3e829edc0a6dd69df35b8ef20c1505
aszlig <aszlig@redmoonstudios.org>
2017-01-04 05:10:47 +0100

    default.nix: Run in a VM if runAllTests is true
    For the nixos_config test, we need to have a writable store and stdenv
    available within that store. We use runInMachine here, so we get the
    same result as if we'd be running without runAllTests.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- default.nix
+++ default.nix
@@ -1,16 +1,29 @@
-{ runAllTests ? false }:
+{ runAllTests ? false, system ? builtins.currentSystem }:
 
 with import <nixpkgs> {};
 
-python3Packages.buildPythonApplication (rec {
-  name = "nixpart-${version}";
-  version = "1.0.0";
+let
+  drv = python3Packages.buildPythonApplication (rec {
+    name = "nixpart-${version}";
+    version = "1.0.0";
 
-  src = ./.;
+    src = ./.;
 
-  checkInputs = [ nix ];
-  propagatedBuildInputs = [ python3Packages.blivet ];
-  makeWrapperArgs = [ "--set GI_TYPELIB_PATH \"$GI_TYPELIB_PATH\"" ];
-} // lib.optionalAttrs runAllTests {
-  NIX_PATH = "nixpkgs=${<nixpkgs>}";
-})
+    checkInputs = [ nix ];
+    propagatedBuildInputs = [ python3Packages.blivet ];
+    makeWrapperArgs = [ "--set GI_TYPELIB_PATH \"$GI_TYPELIB_PATH\"" ];
+  } // lib.optionalAttrs runAllTests {
+    NIX_PATH = "nixpkgs=${<nixpkgs>}";
+  });
+
+  inherit (import <nixpkgs/nixos/lib/testing.nix> {
+    inherit system;
+  }) runInMachine;
+
+in if runAllTests then runInMachine {
+  inherit drv;
+  machine = { lib, pkgs, ... }: {
+    nix.binaryCaches = lib.mkForce [];
+    system.extraDependencies = [ pkgs.stdenv ];
+  };
+} else drv
